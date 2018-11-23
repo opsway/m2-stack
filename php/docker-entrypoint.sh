3d82de294fb5988d3336f95d850b5da5c306b1d9
Oleksiy Biletskiy <olbil@opsway.com>
2018-11-23 11:17:49 +0200

    fix NR

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -2,11 +2,13 @@
 
 if [ -n "${NEWRELIC_LICENSE}" ];
 then
-    rm -Rf "$PHP_INI_DIR/conf.d/100-newrelic.ini"
     docker-php-ext-enable --ini-name 100-newrelic.ini newrelic.so
-    echo "[newrelic]" > "$PHP_INI_DIR/conf.d/100-newrelic.ini"
-    echo "newrelic.license = \"${NEWRELIC_LICENSE}\"" >> "$PHP_INI_DIR/conf.d/100-newrelic.ini"
-    echo "newrelic.appname = \"${NEWRELIC_APPNAME:-PHPapp}\"" >> "$PHP_INI_DIR/conf.d/100-newrelic.ini"
+    rm -Rf "$PHP_INI_DIR/conf.d/100-newrelic.ini"
+    rm -Rf "$PHP_INI_DIR/conf.d/newrelic.ini"
+    echo "[newrelic]" > "$PHP_INI_DIR/conf.d/newrelic.ini"
+    echo 'extension = "newrelic.so"' >> "$PHP_INI_DIR/conf.d/newrelic.ini"
+    echo "newrelic.license = \"${NEWRELIC_LICENSE}\"" >> "$PHP_INI_DIR/conf.d/newrelic.ini"
+    echo "newrelic.appname = \"${NEWRELIC_APPNAME:-PHPapp}\"" >> "$PHP_INI_DIR/conf.d/newrelic.ini"
     newrelic-daemon --pidfile /var/run/newrelic-daemon.pid
 fi
 
