a8966eb40123b4f1d98b37341b1f5d80580b5705
Oleksiy Biletskiy <olbil@opsway.com>
2018-11-27 15:40:16 +0200

    fix varnish config

--- varnish/start.sh
+++ varnish/start.sh
@@ -9,7 +9,7 @@ varnishd -F -a :80 \
    -p http_resp_hdr_len=131072 -p http_resp_size=131072 \
    -p workspace_backend=256k \
    -p workspace_client=256k \
-   -p workspace_thread=8k
+   -p workspace_thread=8k \
    $EXTRA_PARAMS \
    -s $STORAGE$STORAGE_PATH,$STORAGE_SIZE 
 
