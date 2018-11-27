6d5a06d54b56934e2b26c0a06f966cc42a184806
Oleksiy Biletskiy <olbil@opsway.com>
2018-11-27 15:29:07 +0200

    add varnish EXTRA_PARAMS

--- varnish/start.sh
+++ varnish/start.sh
@@ -7,6 +7,10 @@ fi
 varnishd -F -a :80 \
    -f /etc/varnish/default.vcl \
    -p http_resp_hdr_len=131072 -p http_resp_size=131072 \
+   -p workspace_backend=256k \
+   -p workspace_client=256k \
+   -p workspace_thread=8k
+   $EXTRA_PARAMS \
    -s $STORAGE$STORAGE_PATH,$STORAGE_SIZE 
 
 echo "$STORAGE$STORAGE_PATH,$STORAGE_SIZE"
