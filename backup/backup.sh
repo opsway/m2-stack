42b307c3a64d9f229c3d7ebd72de1d7da9542f94
Oleksiy Biletskiy <olbil@opsway.com>
2018-12-05 12:32:09 +0200

    fix typo

--- backup/backup.sh
+++ backup/backup.sh
@@ -4,7 +4,7 @@ BACKUP_DIR=/backups
 if [ ! -d "$BACKUP_DIR" ]; 
 then
     echo "Can't find BACKUP_DIR. Add volume ./some_place:/backup"
-    exit 1;
+    exit 1
 fi
 
 # DB_BACKUP_STORE_PATH=/backup/db/
@@ -26,7 +26,7 @@ if [ -n "${CREATE_DB}" ]; then
     echo "removing old backups ... \n"
     cd ${BACKUP_DIR}/db/ \
     && find ./*.sql.gz -mtime +${DB_BACKUP_STORE_DAYS} -type f -delete
-if
+fi
 
 ## media backup
 # echo "syncing media files ... "
