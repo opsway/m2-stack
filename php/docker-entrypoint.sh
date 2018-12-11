b39d86dcbb1c3459787dc8504f527ff0a79fa6c4
Oleksiy Biletskiy <olbil@opsway.com>
2018-12-11 16:29:36 +0200

    fix tini

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -55,6 +55,6 @@ then
     echo '{ "http-basic": { "repo.magento.com": { "username": "'$COMPOSER_USERNAME'", "password": "'$COMPOSER_PASSWORD'" } } }' > /home/www-data/.composer/auth.json
 fi
 
-tini
+tini "$@"
 
-exec "$@"
+# exec 
