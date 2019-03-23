19c62cfdab711c2ff4537f6f594f6d9836e12c86
Oleksiy Biletskiy <olbil@opsway.com>
2019-03-23 10:25:52 +0200

    add newrelic to php56, add composer to deployer

--- php5.6/docker-entrypoint.sh
+++ php5.6/docker-entrypoint.sh
@@ -5,8 +5,8 @@ then
     rm -Rf "$PHP_INI_DIR/conf.d/100-newrelic.ini"
     docker-php-ext-enable --ini-name 100-newrelic.ini newrelic.so
     echo "[newrelic]" > "$PHP_INI_DIR/conf.d/100-newrelic.ini"
-    echo "newrelic.license = \"${NEWRELIC_LICENSE}\"" >> "$PHP_INI_DIR/conf.d/100-newrelic.ini"
-    echo "newrelic.appname = \"${NEWRELIC_APPNAME:-PHPapp}\"" >> "$PHP_INI_DIR/conf.d/100-newrelic.ini"
+    echo "newrelic.license = \"${NEWRELIC_LICENSE}\"" >> "$PHP_INI_DIR/conf.d/newrelic.ini"
+    echo "newrelic.appname = \"${NEWRELIC_APPNAME:-PHPapp}\"" >> "$PHP_INI_DIR/conf.d/newrelic.ini"
     newrelic-daemon --pidfile /var/run/newrelic-daemon.pid
 fi
 
